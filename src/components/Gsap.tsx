5bf2ee9941e45abe8591d51779e0a663c701d1c1
Kenji Saito <ken-yo@mbr.nifty.com>
2023-01-14 23:09:44 +0900

    Add the GSAP examples

---
+++ src/components/Gsap.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import React, { useEffect, useRef } from 'react';
+import { gsap } from 'gsap';
+
+const Gsap = () => {
+  const textRef = useRef<HTMLDivElement | null>(null);
+
+  const didEffect = useRef(false);
+
+  useEffect(() => {
+    if (didEffect.current) return;
+    didEffect.current = true;
+    gsap.to(textRef.current, {
+      x: '20%',
+      color: 'red',
+    });
+  }, []);
+
+  return <div ref={textRef}>Welcome to GSAP!</div>;
+};
+
+export default Gsap;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+'use client';
 
-const inter = Inter({ subsets: ['latin'] })
+import Head from 'next/head';
+import Image from 'next/image';
+import { Inter } from '@next/font/google';
+import styles from '@/styles/Home.module.css';
+import Link from 'next/link';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
@@ -117,7 +120,14 @@ export default function Home() {
             </p>
           </a>
         </div>
+        <div className={styles.grid}>
+          <Link href="/gsap" className={styles.card}>
+            <h2 className={inter.className}>
+              GSAP <span>-&gt;</span>
+            </h2>
+          </Link>
+        </div>
       </main>
     </>
-  )
+  );
 }
