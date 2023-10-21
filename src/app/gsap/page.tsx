55f47d17934ff244482aa07c221e2f60aba2f3f9
Kenji Saito <ken-yo@mbr.nifty.com>
2023-10-21 14:13:24 +0900

    Bunps same

---
+++ src/app/gsap/page.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { Inter } from "next/font/google";
+import Link from "next/link";
+import styles from "./page.module.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+import Gsap from "@/components/Gsap";
+
+export default function gsap() {
+	return (
+		<>
+			<main className={styles.main}>
+				<div className={styles.description}>
+					<Gsap />
+				</div>
+				<div className={styles.grid}>
+					<Link href="/" className={styles.card}>
+						<h2 className={inter.className}>
+							Home <span>-&gt;</span>
+						</h2>
+					</Link>
+				</div>
+			</main>
+		</>
+	);
+}
--- src/components/Gsap.tsx
+++ src/components/Gsap.tsx
@@ -1,23 +1,23 @@
-'use client';
+"use client";
 
-import React, { useEffect, useRef } from 'react';
-import { gsap } from 'gsap';
+import { gsap } from "gsap";
+import React, { useEffect, useRef } from "react";
 
 const Gsap = () => {
-  const textRef = useRef<HTMLDivElement | null>(null);
+	const textRef = useRef<HTMLDivElement | null>(null);
 
-  const didEffect = useRef(false);
+	const didEffect = useRef(false);
 
-  useEffect(() => {
-    if (didEffect.current) return;
-    didEffect.current = true;
-    gsap.to(textRef.current, {
-      x: '20%',
-      color: 'red',
-    });
-  }, []);
+	useEffect(() => {
+		if (didEffect.current) return;
+		didEffect.current = true;
+		gsap.to(textRef.current, {
+			x: "20%",
+			color: "red",
+		});
+	}, []);
 
-  return <div ref={textRef}>Welcome to GSAP!</div>;
+	return <div ref={textRef}>Welcome to GSAP!</div>;
 };
 
 export default Gsap;
