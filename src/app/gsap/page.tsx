eceb8a1119adb51807b550c81527e4a47d24e4f4
Kenji Saito <ken-yo@mbr.nifty.com>
2024-10-23 00:18:30 +0900

    Bumps node modules

--- src/app/gsap/page.tsx
+++ src/app/gsap/page.tsx
@@ -1,28 +1,28 @@
-"use client";
+'use client';
 
-import { Inter } from "next/font/google";
-import Link from "next/link";
-import styles from "./page.module.css";
+import { Inter } from 'next/font/google';
+import Link from 'next/link';
+import styles from './page.module.css';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
-import Gsap from "@/components/Gsap";
+import Gsap from '@/components/Gsap';
 
 export default function gsap() {
-	return (
-		<>
-			<main className={styles.main}>
-				<div className={styles.description}>
-					<Gsap />
-				</div>
-				<div className={styles.grid}>
-					<Link href="/" className={styles.card}>
-						<h2 className={inter.className}>
-							Home <span>-&gt;</span>
-						</h2>
-					</Link>
-				</div>
-			</main>
-		</>
-	);
+  return (
+    <>
+      <main className={styles.main}>
+        <div className={styles.description}>
+          <Gsap />
+        </div>
+        <div className={styles.grid}>
+          <Link href="/" className={styles.card}>
+            <h2 className={inter.className}>
+              Home <span>-&gt;</span>
+            </h2>
+          </Link>
+        </div>
+      </main>
+    </>
+  );
 }
